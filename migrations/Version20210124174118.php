7a53d0f0f3505b19d82310c4a24d37aa83f446f6
cutshade <josip.jurenec@gmail.com>
2021-01-24 22:37:05 +0100

    Add files via upload

---
+++ migrations/Version20210124174118.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210124174118 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE triangle (id INT AUTO_INCREMENT NOT NULL, a INT NOT NULL, b INT NOT NULL, c INT NOT NULL, opseg INT DEFAULT NULL, povrsina INT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE triangle');
+    }
+}
